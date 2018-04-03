b18f332a8f28d54d76cb3f31305c134a0ae02546
Rustan Corpuz <rustanacexd@gmail.com>
2018-04-04 06:50:22 +0800

    prod config

--- intempio_api/config/production.py
+++ intempio_api/config/production.py
@@ -9,7 +9,7 @@ class Production(Common):
     # https://docs.djangoproject.com/en/2.0/ref/settings/#allowed-hosts
     ALLOWED_HOSTS = ["*"]
     INSTALLED_APPS += ("gunicorn", 'raven.contrib.django.raven_compat')
-    
+
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/2.0/howto/static-files/
     # http://django-storages.readthedocs.org/en/latest/index.html
