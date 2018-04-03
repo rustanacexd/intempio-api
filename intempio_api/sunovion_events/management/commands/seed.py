1c532cb504f3e24e5972ab51543f0078e3db656b
Rustan Corpuz <rustanacexd@gmail.com>
2018-04-04 00:29:59 +0800

    fix seed command

--- intempio_api/sunovion_events/management/commands/seed.py
+++ intempio_api/sunovion_events/management/commands/seed.py
@@ -1,7 +1,6 @@
 from django.core.management.base import BaseCommand
 
 from intempio_api.sunovion_events.factories import SunovionEventFactory
-from intempio_api.users.models import User
 
 
 class Command(BaseCommand):
@@ -10,8 +9,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write("Start seeding data.....")
 
-        # User.objects.create_user(username='antonio', email='antonio@go2impact.com', password='11111111a')
-        User.objects.create_user()
         SunovionEventFactory.create_batch(100)
 
         self.stdout.write(self.style.SUCCESS('Successfully seeded data'))
