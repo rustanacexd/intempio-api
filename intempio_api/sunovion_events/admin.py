38eb6b0baad1a4ffba34478ec8bae2a0817278ca
Rustan Corpuz <rustanacexd@gmail.com>
2018-04-04 08:08:31 +0800

    reduce rows to 20 instead of 100

--- intempio_api/sunovion_events/admin.py
+++ intempio_api/sunovion_events/admin.py
@@ -9,6 +9,7 @@ class SunovionEventAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'created', 'modified', 'reviewed_at', 'accepted_at')
     list_filter = ('status', 'created', 'reviewed_at', 'accepted_at',)
     search_fields = ['id', 'name', 'requestor_name']
+    list_per_page = 20
 
 
 admin.site.register(SunovionEvent, SunovionEventAdmin)
